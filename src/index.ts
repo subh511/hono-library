3f060598c1eeac73d26a52b27a34f69c580b637e
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-05-07 21:48:09 +0530

    added logic for get method for various types of test

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
+// import { Hono } from 'hono'
+
+// const app = new Hono()
+
+// app.get('/', (c) => {
+//   return c.json({
+//     msg: "hello world!"
+//   })
+// })
+
+// app.post('/user', function(c){
+//   return c.text('No No Hono!')
+// })
+
+// export default app
+
+
 import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.json({
-    msg: "hello world!"
-  })
-})
+app.get('/', async (c) => {
+  const body = await c.req.json()
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
 
-app.post('/user', function(c){
-  return c.text('No No Hono!')
+  return c.text('Hello Hono!')
 })
 
 export default app
