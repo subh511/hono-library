e96ddae90b6bac77d83fb0f3987c3bd8a766f492
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-05-07 22:06:03 +0530

    added auth middleware

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,15 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+async function authMiddleware(c:any,next:any){
+  if(c.req.headers("Authorization")){
+    await next()
+  }else{
+    return c.text("You dont have access")
+  }
+}
+app.use(authMiddleware)
+
 app.post('/', async (c) => {
   const body = await c.req.json()
   console.log(body);
